ae53e18ec2df9995c73c50ed508a1f87693a001b
vfbellaver <vfbellaver@gmail.com>
2018-01-25 13:54:48 -0300

    adicionada dependências, adicionadas, entidades, repositories,servicos, utils e classes de testes

---
+++ src/main/java/com/vfbellaver/pontointeligente/api/utils/PasswordUtils.java
@@ -0,0 +1,28 @@
+package com.vfbellaver.pontointeligente.api.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+	private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+	
+	public PasswordUtils() {		
+	}
+	
+	/**
+	 * Gera um hash utilizando BCrypt.
+	 * 
+	 * @param senha
+	 * @return String
+	 */
+	public static String gerarBCrypt(String senha) {
+		if(senha == null) {
+			return senha;
+		}
+		
+		log.info("Gerando hash BCrypt.");
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.encode(senha);
+	}
+}
